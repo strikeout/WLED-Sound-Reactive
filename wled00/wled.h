bf1b4c656f6074f252df8fff16acccc357034c6c
Frank <91616163+softhack007@users.noreply.github.com>
2022-09-20 13:15:07 +0200

    build number up

--- wled00/wled.h
+++ wled00/wled.h
@@ -8,7 +8,7 @@
  */
 
 // version code in format yymmddb (b = daily build)
-#define VERSION 2208081             // WLEDSR specific version
+#define VERSION 2209201             // WLEDSR specific version
 #define SR_VERSION_NAME "0.13.2.1-dev" // WLEDSR version name --> some files need manual updating: package.json, package-lock.json, improv.cpp
 
 #define AC_VERSION 2207021             // AC WLED base version; last updated by PR #217 Merge AC-main into SR-dev
