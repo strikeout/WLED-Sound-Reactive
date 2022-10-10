e3aa60ead6cf4a543a4fd8ffb0dff06d7550e34e
Frank <91616163+softhack007@users.noreply.github.com>
2022-10-10 12:36:20 +0200

    Some bugfixes from upstream 0_14
    button.cpp: fix for shortPressAction; avoid starvation in case strip.IsUpdating is true for too long time.
    udp.cpp: loop otimization and minor bugfixes.
    PlatformIO.ini: upgrade to fixed ESPAsyncWebServer (>= 2.0.7); use 80MHz FLASH speed in all  "Sondreactive" build environments.

--- wled00/button.cpp
+++ wled00/button.cpp
@@ -18,7 +18,7 @@ void shortPressAction(uint8_t b)
   if (!macroButton[b]) {
     switch (b) {
       case 0: toggleOnOff(); stateUpdated(CALL_MODE_BUTTON); break;
-      case 1: ++effectCurrent %= strip.getModeCount(); colorUpdated(CALL_MODE_BUTTON); break;
+      case 1: ++effectCurrent %= strip.getModeCount(); stateChanged = true; colorUpdated(CALL_MODE_BUTTON); break;
     }
   } else {
     applyPreset(macroButton[b], CALL_MODE_BUTTON_PRESET);
@@ -197,10 +197,10 @@ void handleAnalog(uint8_t b)
       // otherwise use "double press" for segment selection
       WS2812FX::Segment& seg = strip.getSegment(macroDoublePress[b]);
       if (aRead == 0) {
-        seg.setOption(SEG_OPTION_ON, 0); // off
+        seg.setOption(SEG_OPTION_ON, false); // off
       } else {
         seg.setOpacity(aRead, macroDoublePress[b]);
-        seg.setOption(SEG_OPTION_ON, 1);
+        seg.setOption(SEG_OPTION_ON, true);
       }
       // this will notify clients of update (websockets,mqtt,etc)
       updateInterfaces(CALL_MODE_BUTTON);
@@ -216,9 +216,13 @@ void handleAnalog(uint8_t b)
 void handleButton()
 {
   static unsigned long lastRead = 0UL;
+  static unsigned long lastRun = 0UL;
   bool analog = false;
+  unsigned long now = millis();
 
-  if (strip.isUpdating()) return; // don't interfere with strip updates. Our button will still be there in 1ms (next cycle)
+  //if (strip.isUpdating()) return; // don't interfere with strip updates. Our button will still be there in 1ms (next cycle)
+  if (strip.isUpdating() && (millis() - lastRun < 200)) return;   // be niced, but avoid button starvation
+  lastRun = millis();
 
   for (uint8_t b=0; b<WLED_MAX_BUTTONS; b++) {
     #ifdef ESP8266
@@ -229,7 +233,7 @@ void handleButton()
 
     if (usermods.handleButton(b)) continue; // did usermod handle buttons
 
-    if ((buttonType[b] == BTN_TYPE_ANALOG || buttonType[b] == BTN_TYPE_ANALOG_INVERTED) && millis() - lastRead > ANALOG_BTN_READ_CYCLE) {   // button is not a button but a potentiometer
+    if ((buttonType[b] == BTN_TYPE_ANALOG || buttonType[b] == BTN_TYPE_ANALOG_INVERTED) && ((now - lastRead) > ANALOG_BTN_READ_CYCLE)) {   // button is not a button but a potentiometer
       analog = true;
       handleAnalog(b); continue;
     }
@@ -242,21 +246,21 @@ void handleButton()
     //momentary button logic
     if (isButtonPressed(b)) { //pressed
 
-      if (!buttonPressedBefore[b]) buttonPressedTime[b] = millis();
+      if (!buttonPressedBefore[b]) buttonPressedTime[b] = now;
       buttonPressedBefore[b] = true;
 
-      if (millis() - buttonPressedTime[b] > WLED_LONG_PRESS) { //long press
+      if (now - buttonPressedTime[b] > WLED_LONG_PRESS) { //long press
         if (!buttonLongPressed[b]) longPressAction(b);
         else if (b) { //repeatable action (~3 times per s) on button > 0
           longPressAction(b);
-          buttonPressedTime[b] = millis() - WLED_LONG_REPEATED_ACTION; //300ms
+          buttonPressedTime[b] = now - WLED_LONG_REPEATED_ACTION; //333ms
         }
         buttonLongPressed[b] = true;
       }
 
     } else if (!isButtonPressed(b) && buttonPressedBefore[b]) { //released
 
-      long dur = millis() - buttonPressedTime[b];
+      long dur = now - buttonPressedTime[b];
       if (dur < WLED_DEBOUNCE_THRESHOLD) {buttonPressedBefore[b] = false; continue;} //too short "press", debounce
       bool doublePress = buttonWaitTime[b]; //did we have a short press before?
       buttonWaitTime[b] = 0;
@@ -270,13 +274,14 @@ void handleButton()
           WLED::instance().initAP(true);
         }
       } else if (!buttonLongPressed[b]) { //short press
-        if (b != 1 && !macroDoublePress[b]) { //don't wait for double press on buttons without a default action if no double press macro set
+        //NOTE: this interferes with double click handling in usermods so usermod needs to implement full button handling
+        if ((b != 1) && !macroDoublePress[b]) { //don't wait for double press on buttons without a default action if no double press macro set
           shortPressAction(b);
         } else { //double press if less than 350 ms between current press and previous short press release (buttonWaitTime!=0)
           if (doublePress) {
             doublePressAction(b);
           } else {
-            buttonWaitTime[b] = millis();
+            buttonWaitTime[b] = now;
           }
         }
       }
@@ -285,12 +290,12 @@ void handleButton()
     }
 
     //if 350ms elapsed since last short press release it is a short press
-    if (buttonWaitTime[b] && millis() - buttonWaitTime[b] > WLED_DOUBLE_PRESS && !buttonPressedBefore[b]) {
+    if (buttonWaitTime[b] && ((now - buttonWaitTime[b]) > WLED_DOUBLE_PRESS) && !buttonPressedBefore[b]) {
       buttonWaitTime[b] = 0;
       shortPressAction(b);
     }
   }
-  if (analog) lastRead = millis();
+  if (analog) lastRead = now;
 }
 
 void handleIO()
